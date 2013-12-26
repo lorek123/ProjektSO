a78fb4775b7406710654685cf10099b2507c4072
lorek123 <lorek123@gmail.com>
2013-12-26 19:09:05 +0100

    Bloby i ogólny zarys modułu

---
+++ Modul Nadzorczy/Interpreter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_Nadzorczy
+{
+    class Interpreter
+    {
+        void dodaj(int a) { }
+        void odejmij(int a) { }
+        void pomnoz(int a) { }
+        void podziel(int a) { }
+        void zeruj() { }
+        void zakoncz_blad() { }
+        void zakoncz() { }
+        void skok_zero() { }
+        void skok_nzero() { }
+        void wypisz(string a) { }
+    }
+}
---
+++ Modul Nadzorczy/Modul/Ladowanie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_Nadzorczy
+{
+    class Ladowanie
+    {
+        void Ladowanie() { }
+        
+        void job() { }
+        void scan() { }
+        void expunge() { }
+        void load() { }
+    }
+}
