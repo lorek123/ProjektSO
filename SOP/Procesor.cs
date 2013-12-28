53571b19651ff6a33ac002406f7cd7e3c1de0dd6
Laionik <lukasz.sosinski4@gmail.com>
2013-12-28 15:47:58 +0100

    Gettery i settery dla Lorka

--- SOP/Procesor.cs
+++ SOP/Procesor.cs
@@ -8,10 +8,65 @@ namespace Proces_modul_nizszy
 {
     class Procesor
     {
-        public int r0;
-        public int r1;
-        public int r2;
-        public int r3;
-        public int r4;
+        private int r0;
+        private int r1;
+        private int r2;
+        private int r3;
+        private int r4;
+
+        /*getery*/
+        int get_r0()
+        {
+            return r0;
+        }
+
+        int get_r1()
+        {
+            return r1;
+        }
+
+        int get_r2()
+        {
+            return r2;
+        }
+
+        int get_r3()
+        {
+            return r3;
+        }
+
+        int get_r4()
+        {
+            return r4;
+        }
+
+
+        /*settery*/
+        void set_r0(int value)
+        {
+            r0 = value;
+        }
+
+        void set_r1(int value)
+        {
+            r1 = value;
+        }
+
+        void set_r2(int value)
+        {
+            r2 = value;
+        }
+
+        void set_r3(int value)
+        {
+            r3 = value;
+        }
+
+        void set_r4(int value)
+        {
+            r4 = value;
+        }
+
+
     }
 }
