13ec54f6665475ea48b053402c2f89092f58c0dd
lorek123 <lorek123@gmail.com>
2014-01-03 21:14:40 +0100

    minimalnie zrobiona obsluga wirtualnego procesora

--- Modul Nadzorczy/Interpreter.cs
+++ Modul Nadzorczy/Interpreter.cs
@@ -8,15 +8,33 @@ namespace Modul_Nadzorczy
 {
     class Interpreter
     {
-        void dodaj(int a) { }
-        void odejmij(int a) { }
-        void pomnoz(int a) { }
-        void podziel(int a) { }
-        void zeruj() { }
-        void zakoncz_blad() { }
+        void dodaj(int a) {
+            Procesor.set_r0(Procesor.get_r0() + a);
+        }
+        void odejmij(int a) { 
+            Procesor.set_r0(Procesor.get_r0() - a);
+        }
+        void pomnoz(int a) {
+            Procesor.set_r0(Procesor.get_r0() * a);
+        }
+        void podziel(int a) {
+            try
+            {
+                Procesor.set_r0(Procesor.get_r0() / a);
+            }
+            catch (DivideByZeroException ex)
+            {
+                zakoncz_blad();
+            }
+
+        }
+        void zeruj() {
+            Procesor.set_r0(0);
+        }
+        void zakoncz_blad(Exception Exception) { }
         void zakoncz() { }
-        void skok_zero() { }
-        void skok_nzero() { }
+        void skok_zero(string a) { }
+        void skok_nzero(string a) { }
         void wypisz(string a) { }
     }
 }
