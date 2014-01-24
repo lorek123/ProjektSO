8f6efb4538963108de312550ca8bb3235074e930
lorek123 <lorek123@gmail.com>
2014-01-24 15:03:37 +0100

    -;.-

--- ProjektSOFULL/Program.cs
+++ ProjektSOFULL/Program.cs
@@ -38,6 +38,9 @@ namespace ProjektSOFULL
                 planista = new SRT_zawiadowca(ref example, ref CPU_man.grupy_procesow);*/
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 068e7fd3a438505560e56768747bda8a64b3d942
+=======
 >>>>>>> 068e7fd3a438505560e56768747bda8a64b3d942
 =======
 >>>>>>> 068e7fd3a438505560e56768747bda8a64b3d942
